8caab86af7b7351dc5464a804ac9b4e472203e41
ToiTenSon <23010313@st.phenikaa-uni.edu.vn>
2025-10-03 17:05:43 +0700

    siu

--- library_management/accounts/urls.py
+++ library_management/accounts/urls.py
@@ -12,6 +12,12 @@ urlpatterns = [
     path('catalog/', views.catalog, name='catalog'), 
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
+    path('upgrade/<str:level>/', views.upgrade_membership, name='upgrade_membership'),
+    path("membership/", views.membership, name="membership"),
+    path("payment/", views.payment, name="payment"),
+    path("process-payment/", views.process_payment, name="process_payment"),
+    path('payment_done/', views.payment_done, name='payment_done'),
+
     path('users/<int:user_id>/edit/', views.edit_user, name='edit_user'),
     path("api/users/<int:user_id>/delete/", views.delete_user_api, name="delete_user_api"),
     path("api/add-book/", views.add_book, name="add_book"),
